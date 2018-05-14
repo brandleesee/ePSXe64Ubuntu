b6d297a31a9ad451498a5c989854406b4c81d630
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2018-05-14 23:26:34 +0200

    v10

--- ePSXe64Ubuntu.sh
+++ ePSXe64Ubuntu.sh
@@ -20,7 +20,7 @@ PS3="Choose from 1 to 3 above. "
 tput setaf 2; echo "Welcome to ePSXe64Ubuntu.sh script, $ver."; tput sgr0
 tput setaf 1; echo "When ePSXe GUI appears on screen:"; tput sgr0
 tput setaf 1; echo "  Right click on icon in Dash/Dock/Panel"; tput sgr0
-tput setaf 1; echo "  Add to Favorities/Lock"; tput sgr0
+tput setaf 1; echo "  Add to Favorites/Lock"; tput sgr0
 tput setaf 1; echo "  CLOSE ePSXe GUI to continue with the script."; tput sgr0
 tput setaf 2; echo "Script started."; tput sgr0
 
